6268123da5f74203e3341245919aeb7129083ab3
Zack Fisch <zack@zfisch.com>
2014-06-17 23:01:41 -0400

    added/ playing around with Structs, allowd for creation of rooms

--- Dungeon.rb
+++ Dungeon.rb
@@ -6,22 +6,10 @@ class Dungeon
 		@rooms = []
 	end
 
-	class Player
-		attr_accessor :name, location
-
-		def initialize(player_name)
-			@name = player_name
-		end
+	def add_room(reference, name, description, connections)
+		@rooms << Room.new(reference, name description, connections)
 	end
 
-	class Room
-		attr_accessor :reference, :name, :description, :connections
-
-		def initialize(reference, name, description, connections)
-			@reference = reference
-			@name = name
-			@description = description
-			@connections = connections
-		end
-	end
+	Player = Struct.new(:name, :location)
+	Room = Struct.new(:reference, :name, :description, :connections) 
 end
